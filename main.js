9ae5dc513902ca9986be864d3eaeb76d6631f8e5
831758 <98672989+831758@users.noreply.github.com>
2022-02-18 17:54:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+x = 0;
+y = 0;
+
+draw_circle = "";
+draw_rectangle = "";
+var speechrecognition = window.webkitSpeechRecognition;
+var recognition = new speechrecognition();
+
+function setup() {
+    canvas = createCanvas(900, 600)
+}
+
+function start() {
+    document.getElementById("status").innerHTML = "System Is Listening, Please Speak";
+    recognition.start()
+}
+
+recognition.onresult = function (event) {
+    console.log(event);
+    var content = event.results[0][0].transcript
+    document.getElementById("status").innerHTML = "The Speech Has Been Recognized As " + content
+    if(content=="Circle."){
+        x=Math.floor(Math.random()*900)
+        y=Math.floor(Math.random()*600)
+        console.log("Started Drawing Circle")
+        document.getElementById("status").innerHTML="Started Drawing Circle"
+        draw_circle="set"
+    }
+
+    if(content=="Rectangle."){
+        x=Math.floor(Math.random()*900)
+        y=Math.floor(Math.random()*600)
+        document.getElementById("status").innerHTML="Started Drawing Rectangle"
+        draw_rectangle="set"
+    }
+}
+
+function draw(){
+    if(draw_circle=="set"){
+        radius=Math.floor(Math.random()*100)
+        circle(x, y, radius)
+        document.getElementById("status").innerHTML="Circle Is Drawn"
+        draw_circle=""
+    }
+
+    if(draw_rectangle=="set"){
+        rect(x, y, 70, 50)
+        document.getElementById("status").innerHTML="Rectangle Is Drawn"
+        draw_rectangle=""
+    }
+}
